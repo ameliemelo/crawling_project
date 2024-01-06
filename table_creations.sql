a86f1854f4b0d3d10ffca26f424b576436110eb0
Mehdi-Inane <inaneahmedmehdi@gmail.com>
2024-01-06 18:42:25 +0100

    setting up code for finalizing the scraping of author and ref info

---
+++ table_creations.sql
@@ -0,0 +1,31 @@
+CREATE TABLE articles (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year INTEGER,
+    url TEXT,
+    abstract TEXT,
+    location TEXT
+);
+
+CREATE TABLE authors (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT, -- Normally one would enforce NON Null constraints, but not enforced here since extraction is made from GROBID
+    middle_name TEXT,
+    email TEXT UNIQUE
+);
+
+CREATE TABLE written_by (
+    article_id INTEGER,
+    author_id INTEGER,
+    FOREIGN KEY (article_id) REFERENCES articles(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id),
+    UNIQUE (article_id, author_id)
+);
+
+CREATE TABLE citations (
+    article_id INTEGER,
+    title TEXT,
+    authors TEXT,
+    FOREIGN KEY (article_id) REFERENCES articles(id),
+);
